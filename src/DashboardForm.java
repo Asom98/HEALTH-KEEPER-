28fec7a91e75af6ab938e8cd18598e6d394e41e1
Simon Eriksson <redundantinfo@gmail.com>
2022-04-27 20:21:50 +0200

    more stuff

---
+++ src/DashboardForm.java
@@ -0,0 +1,117 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DashboardForm extends JFrame{
+    private JPanel dashboardPanel;
+    private JButton btnRegister;
+    private JLabel lbDashboard;
+
+
+    public DashboardForm(){
+        setTitle("Dashboard");
+        setContentPane(dashboardPanel);
+        setMinimumSize(new Dimension(500, 430));
+        setSize(1200, 700);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        boolean hasRegisteredUsers = connectToDatabase();
+        if (hasRegisteredUsers){
+            //show login form
+            LoginForm loginForm = new LoginForm(this);
+            User user = loginForm.user;
+
+            if (user != null) {
+                lbDashboard.setText("User: " + user.name);
+                setLocationRelativeTo(null);
+                setVisible(true);
+            }
+            else {
+                dispose();
+            }
+        }
+        else {
+            // display registration
+            RegistrationForm registrationForm = new RegistrationForm(this);
+            User user = registrationForm.user;
+
+            if (user != null){
+                lbDashboard.setText("User: " + user.name);
+                setLocationRelativeTo(null);
+                setVisible(true);
+            }
+            else {
+                dispose();
+            }
+        }
+        btnRegister.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RegistrationForm registrationForm = new RegistrationForm(DashboardForm.this);
+                User user = registrationForm.user;
+
+                if (user != null){
+                    JOptionPane.showMessageDialog(DashboardForm.this,
+                            "New user: " + user.name,
+                            "Registration successful",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+    }
+
+    private boolean connectToDatabase(){
+        boolean hasRegisteredUsers = false;
+
+        final String MYSQL_SERVER_URL = "jdbc:mysql://localhost/";
+        final String DB_URL = "jdbc:mysql://localhost/agileMethods?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "root";
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            Statement statement = conn.createStatement();
+            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS agileMethods");
+            statement.close();
+            conn.close();
+
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            statement = conn.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS users("
+                    + "id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,"
+                    + "name VARCHAR(50) NOT NULL,"
+                    + "email VARCHAR(50) NOT NULL UNIQUE,"
+                    + "password VARCHAR(50) NOT NULL"
+                    +")";
+            statement.executeUpdate(sql);
+
+            // check for users
+            statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM users");
+
+            if (resultSet.next()) {
+                int numUsers = resultSet.getInt(1);
+                if (numUsers > 0){
+                    hasRegisteredUsers = true;
+                }
+            }
+
+            statement.close();
+            conn.close();
+        }
+
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return hasRegisteredUsers;
+    }
+
+    public static void main(String[] args) {
+        DashboardForm myForm = new DashboardForm();
+    }
+}
---
+++ src/LoginForm.java
@@ -0,0 +1,105 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class LoginForm extends JDialog{
+    private JTextField tfEmail;
+    private JPasswordField pfPassword;
+    private JButton btnLogin;
+    private JButton btnCancel;
+    private JPanel loginPanel;
+
+
+    public LoginForm(JFrame parent){
+        super(parent);
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(450, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+
+
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email = tfEmail.getText();
+                String password = String.valueOf(pfPassword.getPassword());
+
+                user = getAuthenticatedUser(email, password);
+
+                if (user != null){
+                    dispose();
+                }
+                else {
+                    JOptionPane.showMessageDialog(LoginForm.this,
+                            "Invalid email or password",
+                            "Try again",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public User user;
+    private User getAuthenticatedUser(String email, String password) {
+        User user = null;
+
+        final String DB_URL = "jdbc:mysql://localhost/agileMethods?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "root";
+
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement prepStatement = conn.prepareStatement(sql);
+            prepStatement.setString(1, email);
+            prepStatement.setString(2, password);
+
+            ResultSet resultSet = prepStatement.executeQuery();
+
+            if (resultSet.next()){
+                user = new User();
+                user.name = resultSet.getString("name");
+                user.email = resultSet.getString("email");
+                user.password = resultSet.getString("password");
+            }
+
+            stmt.close();
+            conn.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+
+    public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm(null);
+        User user = loginForm.user;
+        if (user != null) {
+            System.out.printf("Login successful. \nWelcome " + user.name);
+            System.out.printf("\nEmail: " + user.email);
+        }
+        else{
+            System.out.println("Login cancelled");
+        }
+    }
+}
